c86f130c36da4b401aeb026962ccfc802aa305aa
Christoph Lukas <lukas@pingworks.de>
2015-11-05 14:50:45 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'Apache 2.0'
 description      'Installs/Configures pw_mofa'
 long_description 'Installs/Configures pw_mofa'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'pw_chefdk'
